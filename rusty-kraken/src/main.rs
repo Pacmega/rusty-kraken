028d305eac0ff922d327276bf595b6ea30440107
Pacmega <bas.witters@Hotmail.com>
2024-03-23 21:31:14 +0100

    Receiving price info, JSON as text

--- rusty-kraken/src/main.rs
+++ rusty-kraken/src/main.rs
@@ -1,3 +1,15 @@
+use reqwest::blocking::Client;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Time to ask Kraken a question!");
+    let url = "https://api.kraken.com/0/public/OHLC?pair=ETHEUR";
+    let client: Client = Client::new();
+    // TODO: do not use unwrap().
+    // TODO: now it's a string instead of a JSON, but the response is a JSON.
+    let resp = client
+        .get(url)
+        .send()
+        .unwrap()
+        .text();
+    print!("Response code: {:?}", resp);
 }
